edaf83a19dd6da70019a0b94327852023dfc5e3d
Gabriel Freire <gabrielfvale@gmail.com>
2021-04-07 12:54:55 -0300

    Add scene skeleton

---
+++ Scene.lua
@@ -0,0 +1,22 @@
+local class = require("class")
+local S = class:derive("Scene")
+
+function S:new( ... )
+end
+
+function S:enter()
+end
+
+function S:update( dt )
+end
+
+function S:draw()
+end
+
+function S:destroy()
+end
+
+function S:exit()
+end
+
+return S
---
+++ SceneManager.lua
@@ -0,0 +1,26 @@
+local class = require("class")
+
+local SM = class:derive("SceneManager")
+
+function SM:new( ... )
+end
+
+function SM:add( ... )
+end
+
+function SM:remove( ... )
+end
+
+function SM:switch( ... )
+end
+
+function SM:pop( ... )
+end
+
+function SM:update( dt )
+end
+
+function SM:draw( ... )
+end
+
+return SM
